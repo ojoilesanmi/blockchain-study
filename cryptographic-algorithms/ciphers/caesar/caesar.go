24e7b7e6d1920a1a827b3e1d09dc94b4e1f61679
MacBobby Chibuzor <macbobbychibuzor@MacBobbys-MacBook-Pro.local>
2022-12-08 10:57:25 +0100

    made deletions

--- cryptographic-algorithms/ciphers/caesar/caesar.go
+++ cryptographic-algorithms/ciphers/caesar/caesar.go
@@ -1,8 +1,12 @@
 package ciphers
 
+import (
+    
+)
+
 // Encrypt function receives an input string and a key for shifting with, and return a string
 func Encrypt(input string, key int) string {
-    // logic here
+
 }
 
 func Decrypt(input string, key int) string {
